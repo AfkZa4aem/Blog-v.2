d032425dc13ba751f0dac40273dfcd050db31716
AfkZa4aem <nastypinagrably@gmail.com>
2017-04-13 15:51:47 +0300

    Add user microposts without aws integration

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,6 +8,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
+  # if Rails.env.production?
+  #   storage :for
+  # else
+  #   storage :file
+  # end  
   storage :file
   # storage :fog
 
