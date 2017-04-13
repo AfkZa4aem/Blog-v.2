f7306bd850c1f2de43ed15e94809f433b082e6cc
AfkZa4aem <nastypinagrably@gmail.com>
2017-04-13 12:58:53 +0300

    Update micropost interface spec

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:thitanan)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select "div.pagination"
+    # wrong info
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, micropost: { content: "" }
+    end
+    assert_select 'div#error_explanation' 
+    # right info
+    content = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, micropost: { content: content }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    # delete micropost
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    # another user profile
+    get user_path(users(:maxim))
+    assert_select 'a', text: 'delete', count: 0
+  end
 end
